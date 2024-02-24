a6aed3151ef48f50dc8c9a9ec0731e80db68045f
AhmedWaled <67336269+LiNePasha@users.noreply.github.com>
2024-02-24 13:16:24 +0200

    new Component Handler
    new Component Handler

---
+++ App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import DateRangePicker from './components/DateRangePicker/DateRangePicker';
+import { useState } from 'react';
+
+const App: React.FC = () => {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
+  const handleDateRangeChange = (start: Date, end: Date) => {
+    setStartDate(start);
+    setEndDate(end);
+};
+
+  // array of dynamic ranges
+  const dynamicRanges = [7, 14, 30, 90];
+
+  return (
+    <div className="App">
+      <header>
+          {/* Swypex Logo */}
+          <img 
+            src='https://res.cloudinary.com/dzxbbqq4l/image/upload/v1708730288/2oMLjXEOV94NTYr02cFAg7ldk_1_taudli.webp' 
+            alt='Swypex Logo' 
+          />
+          <span>Date Picker</span>
+      </header>
+
+      <main>
+        <DateRangePicker 
+          onChange={handleDateRangeChange}
+          startDate={startDate}
+          endDate={endDate}
+          dynamicRanges={dynamicRanges} // dynamic ranges
+          className="custom-date-picker" // Add custom class name
+          rangesButtonStyle={{ background: '#001684', color: '#fff' }} // Add custom button background color
+          style={{  }} // Add custom inline styles
+        />
+
+      </main>
+
+
+    </div>
+  );
+};
+
+export default App;
