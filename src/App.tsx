ea5add35a43e3548346468504f2e3848420ab19c
AhmedWaled <67336269+LiNePasha@users.noreply.github.com>
2024-02-24 19:05:07 +0200

    good component , clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DateRangePicker from './components/DateRangePicker/DateRangePicker';
+import { fetchExchangeRates } from './utils/api';
+import { formatDate } from './utils/dateUtils';
+import TableRates from './components/TableRates';
 
 const App: React.FC = () => {
+  const [exchangeRates, setExchangeRates] = useState<{ [date: string]: { USDEGP: number; USDCAD: number } } | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async (startDate: Date, endDate: Date) => {
+    setLoading(true);
+    try {
+      const response = await fetchExchangeRates(formatDate(startDate), formatDate(endDate));
+      if (response && response.error) {
+        setError('API Plan HTTP Access Restricted, please Try Again ( you can open another browser ).');
+      } else {
+        setExchangeRates(response?.quotes || null);
+      }
+    } catch (error) {
+      console.error('Error fetching exchange rates:', error);
+      setError('Error fetching exchange rates. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDateRangeChange = (startDate: Date, endDate: Date) => {
+    fetchData(startDate, endDate);
+  };
 
   // array of dynamic ranges
   const dynamicRanges = [7, 14, 30, 90];
@@ -18,13 +45,21 @@ const App: React.FC = () => {
       </header>
 
       <main>
+        <h1>Click to open Exchange Rates</h1>
         <DateRangePicker 
+          onChange={handleDateRangeChange}
           dynamicRanges={dynamicRanges} // dynamic ranges
           className="custom-date-picker" // Add custom class name
           rangesButtonStyle={{ background: '#001684', color: '#fff' }} // Add custom button background color
           style={{  }} // Add custom inline styles
+          fetchData={fetchData} // Pass the fetchData function as prop
         />
       </main>
+
+      {loading && <div className="loading-indicator">Loading...</div>}
+      {error && <p className="error-message">{error}</p>}
+
+      {exchangeRates && <TableRates exchangeRates={exchangeRates} />}
     </div>
   );
 };
